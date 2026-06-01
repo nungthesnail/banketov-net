e033ee18d1335009133f34eb889ed57f21a79f45
nungthesnail <nungthesnail@mail.ru>
2026-06-01 20:11:38 +0500

    Management classes, config and readme - created classes to manage app: App and View; - created config example; - created readme and gitignore;

---
+++ bootstrap.php
@@ -0,0 +1,76 @@
+<?php
+
+session_start();
+
+class App {
+    private ?array $config = null;
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getConfig(): array {
+        if ($this->config)
+            return $this->config;
+
+        $this->config = parse_ini_file(__DIR__ . '/config.ini');
+        if (!$this->config)
+            die('Cannot load configuration');
+        return $this->config;
+    }
+
+    /**
+     * @return mysqli
+     */
+    public function getConnection(): mysqli {
+        $cfg = $this->getConfig();
+        $conn = new mysqli();
+        $success = $conn->connect(
+            $cfg['DB_HOST'],
+            $cfg['DB_USER'],
+            $cfg['DB_PASSWORD'],
+            $cfg['DB_DATABASE'],
+            $cfg['DB_PORT']);
+        
+        if (!$success)
+            die('Cannot connect to db');
+        return $conn;
+    }
+
+    /**
+     * @return null|array<string, mixed>
+     */
+    public function getUserInfo(): array|null {
+        if (empty($_SESSION['userId']) || empty($_SESSION['isAdmin']))
+            return null;
+
+        return [
+            'userId' => $_SESSION['userId'],
+            'isAdmin' => $_SESSION['isAdmin']
+        ];
+    }
+}
+
+class View {
+    /**
+     * @return void
+     */
+    public function flash(?string $message = null): void {
+        if ($message) {
+            $_SESSION['flash'] = $message;
+        } else {
+            if (isset($_SESSION['flash'])) { ?>
+                <div class="text-danger"><?= $_SESSION['flash'] ?></div>
+            <?php }
+            unset($_SESSION['flash']);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function reloadWithFlash(string $message, string $location): void {
+        $this->flash($message);
+        header("Location: $location");
+        exit();
+    }
+}
